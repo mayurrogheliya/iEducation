71ebf5aa0adf7f035ce3b2c75ad18afa97904ec1
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-04-04 06:38:18 +0530

    no change

--- home.php
+++ home.php
@@ -9,24 +9,24 @@
 
 <body>
     <?php
-        include_once("header.php");
+    include_once("header.php");
     ?>
     <section class="py-5 py-xl-8" id="home">
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6 col-12 d-flex flex-column justify-content-center align-items-start order-md-0 order-1">
-                <h1 class="fw-bold mb-4" style="font-size: 2.5rem;">Knowledge is Power, and It Begins with Education.</h1>
-                <h4 class="mb-4" style="line-height: 1.8;">Empower through knowledge! iEducation sparks brilliance with the finest online learning opportunities. Unleash your potential today!</h4>
-                <a class="btn btn-primary fs-5" href="course.php">Get Started <i class="fa fa-arrow-right"></i></a>
-            </div>
-            <div class="col-md-6 col-12 d-flex justify-content-center align-items-center order-md-1 order-0">
-                <figure>
-                    <img src="img/homeImage.gif" class="img-fluid" alt="Responsive Image">
-                </figure>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6 col-12 d-flex flex-column justify-content-center align-items-start order-md-0 order-1">
+                    <h1 class="fw-bold mb-4" style="font-size: 2.5rem;">Knowledge is Power, and It Begins with Education.</h1>
+                    <h4 class="mb-4" style="line-height: 1.8;">Empower through knowledge! iEducation sparks brilliance with the finest online learning opportunities. Unleash your potential today!</h4>
+                    <a class="btn btn-primary fs-5" href="course.php">Get Started <i class="fa fa-arrow-right"></i></a>
+                </div>
+                <div class="col-md-6 col-12 d-flex justify-content-center align-items-center order-md-1 order-0">
+                    <figure>
+                        <img src="img/homeImage.gif" class="img-fluid" alt="Responsive Image">
+                    </figure>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 
 </body>
